24f87f5639e4a1b34a9147b47e6929ca1dacf7e5
BigWilliam <william@d3fy.com>
2017-01-26 02:55:45 -0700

    replace $ with jQuery

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,20 +1,19 @@
-$(window).load(function(){
-  var $grid = $('.grid').isotope({
+jQuery(window).load(function(){
+  var $grid = jQuery('.grid').isotope({
     itemSelector: '.d3fy-item',
     transitionDuration: '0.6s',
     masonry: {
       columnWidth: 0
     }
   });
-  $('.portfolio-filters-inline').on('click', 'a', function() {
-    $('.portfolio-filters-inline a').removeClass("current");
+  jQuery('.portfolio-filters-inline').on('click', 'a', function() {
+    jQuery('.portfolio-filters-inline a').removeClass("current");
 
     //adds the class to whichever item you clicked
-    $(this).addClass("current");
-    var filterValue = $(this).attr('data-filter');
+    jQuery(this).addClass("current");
+    var filterValue = jQuery(this).attr('data-filter');
     $grid.isotope({
       filter: filterValue
     });
   });
-
 });
