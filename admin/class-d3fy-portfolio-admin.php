ff267d1e61dc86e452f7ed9180b286c13e65f779
Justin Tucker <justin@certainstrings.com>
2017-01-30 21:03:46 -0700

    File and comment cleanup

--- admin/class-d3fy-portfolio-admin.php
+++ admin/class-d3fy-portfolio-admin.php
@@ -82,18 +82,6 @@ class D3fy_Portfolio_Admin {
 	 */
 	public function enqueue_styles() {
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in D3fy_Portfolio_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The D3fy_Portfolio_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
 		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/d3fy-portfolio-admin.css', array(), $this->version, 'all' );
 
 	}
@@ -104,18 +92,6 @@ class D3fy_Portfolio_Admin {
 	 * @since    1.0.0
 	 */
 	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in D3fy_Portfolio_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The D3fy_Portfolio_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
 
 		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/d3fy-portfolio-admin.js', array( 'jquery' ), $this->version, false );
 
